dbcc958d95bee651adbc5fc5f14000918cf05e49
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2022-06-18 16:22:27 +0900

    create file

---
+++ src/space/yurisi/pluginname/mahjong/Agari.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace space\yurisi\pluginname\mahjong;
+
+class Agari {
+  
+  public function __construct($yaku,$arrayHai,$janto,$mentsu1,$mentsu2,$mentsu3,$mentsu4){
+    $this->yaku = $yaku;
+    $this->arrayHai = $arrayHai;
+    $this->janto = $janto;
+    $this->mentsu1 = $mentsu1;
+    $this->mentsu2 = $mentsu2;
+    $this->mentsu3 = $mentsu3;
+    $this->mentsu4 = $mentsu4;
+  }
+  
+}
