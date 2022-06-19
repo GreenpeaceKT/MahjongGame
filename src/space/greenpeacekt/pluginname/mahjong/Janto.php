c75850d04fbf493a40aaa20004aa93fd4f972138
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2022-06-19 22:43:54 +0900

    create janto

---
+++ src/space/greenpeacekt/pluginname/mahjong/Janto.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace space\greenpeacekt\pluginname\mahjong;
+
+class Janto {
+  public function __construct($arrayHai){
+    $this->arrayHai = $arrayHai;
+  }
+}
